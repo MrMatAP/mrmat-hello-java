e082b7f47b8f4c7f23a99c11b7f37cb96d689ee5
Mathieu Imfeld <imfeldma@gmail.com>
2017-05-14 11:48:20 +0800

    Initial project structure

---
+++ spring-concurrency/src/main/java/org/mrmat/hello/java/spring/concurrency/MrmatHelloJavaApplication.java
@@ -0,0 +1,12 @@
+package org.mrmat.hello.java.spring.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrmatHelloJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrmatHelloJavaApplication.class, args);
+	}
+}
