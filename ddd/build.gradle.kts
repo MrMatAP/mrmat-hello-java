0711d7e37828b855eff3185ce0051a3ca9558e43
Mathieu Imfeld <imfeldma@gmail.com>
2025-08-10 10:11:02 +0200

    Added testsuite and made versioning re-usable

--- ddd/build.gradle.kts
+++ ddd/build.gradle.kts
@@ -3,3 +3,18 @@ plugins {
 }
 
 group = "org.mrmat.hello.java"
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        events("passed")
+    }
+}
---
+++ ddd/src/test/java/test/DDDTests.java
@@ -0,0 +1,12 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class DDDTests {
+
+    @Test
+    void alwaysTrue() {
+        assertTrue(true);
+    }
+}
